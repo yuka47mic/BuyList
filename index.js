d882ae0e4cebad2e9163d497888db2aba20ab2f0
yuka <okayu4747@yahoo.co.jp>
2024-01-19 23:51:08 +0900

    一覧画面仮実装

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("list.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
