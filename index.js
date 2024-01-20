27dad1946fe87ea9fcf392d7c4503fdd118b0d35
yuka <okayu4747@yahoo.co.jp>
2024-01-21 00:14:06 +0900

    編集画面仮実装

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/create', (req, res) => {
   res.render('create.ejs');
 });
 
+// 編集画面
+app.get('/edit/:id', (req, res) => {
+  res.render('edit.ejs');
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
