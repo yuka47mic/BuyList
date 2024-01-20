bf13e733a736bad19294784d830664944d32da42
yuka <okayu4747@yahoo.co.jp>
2024-01-21 00:04:25 +0900

    新規作成画面仮実装

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-  res.render("list.ejs");
+// 一覧画面
+app.get('/', (req, res) => {
+  res.render('list.ejs');
+});
+
+// 新規作成画面
+app.get('/create', (req, res) => {
+  res.render('create.ejs');
 });
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log('Server listening on port 3000');
 });
