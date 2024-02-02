29842085ebdbb5467ce8b6059643d155946240ac
yuka <okayu4747@yahoo.co.jp>
2024-02-02 21:01:05 +0900

    一覧画面実装

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'rootroot',
+  database: 'memo',
+});
 
 // 一覧画面
 app.get('/', (req, res) => {
-  res.render('list.ejs');
+  try {
+    connection.query('SELECT * FROM memo', (error, results) => {
+      res.render('list.ejs', { items: results });
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 // 新規作成画面
